fded21fb6ca681528e731cc27f53b28d565b130a
mendsalbert <mendsalbert@gmail.com>
2022-10-26 00:03:33 +0000

    cropapp

---
+++ src/components/homeComponents/CalltoActionSection.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const CalltoActionSection = () => {
+  return (
+    <div className="subscribe-section bg-with-black">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <div className="subscribe-head">
+              <h2>Do you want quality access to farming tools?</h2>
+              <p>get access to quality farming tools here</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalltoActionSection;
